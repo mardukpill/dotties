3c26772547327cfbad4a4d6392e0c898da58a1f5
marduk <mike@luxvera.com>
2024-08-07 01:26:35 -0400

    nvim: rename keys in which-key

--- modules/home/cli/neovim/plugins/which.nix
+++ modules/home/cli/neovim/plugins/which.nix
@@ -1,6 +1,18 @@
 _: {
-  programs.nixvim.plugins.which-key = {
-    enable = true;
-    showKeys = true;
+  programs.nixvim.plugins = {
+    which-key = {
+      enable = true;
+      showKeys = true;
+      keyLabels = {
+        "<space>" = "SPC";
+        "<leader>" = "SPC";
+        "<cr>" = "ENTER";
+        "<CR>" = "ENTER";
+        "<tab>" = "TAB";
+        "<TAB>" = "TAB";
+        "<bs>" = "BACKSPACE";
+        "<BS>" = "BACKSPACE";
+      };
+    };
   };
 }
