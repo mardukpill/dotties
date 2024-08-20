5d02e65c84229946638798fb29431e27199cce8a
marduk <mike@luxvera.com>
2024-08-20 12:48:31 -0400

    stirps: enable openssh

--- systems/x86_64-linux/stirps/default.nix
+++ systems/x86_64-linux/stirps/default.nix
@@ -20,6 +20,15 @@ in
     ./hardware-configuration.nix
   ];
 
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+    };
+  };
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGsgZIUxHRpzYHhLfa2CNmZnuyr5omNyK5rbSWeBnqY3 mike@blade"
+  ];
   networking.firewall.checkReversePath = false;
 
   dotties = {
